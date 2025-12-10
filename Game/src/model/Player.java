ba6123c6cd5779ab4a480247f275a8ac9f503b1b
EldiRangel <eldi456.rangel@gmail.com>
2025-12-10 14:26:35 -0400

    Implemtacion player class

--- Game/src/model/Player.java
+++ Game/src/model/Player.java
@@ -1,3 +1,34 @@
+package model;
+
 public class Player {
+    private String name;
+    private int score;
+    private int lives;
     
+    public Player(String name) {
+        this.name = name;
+        this.score = 0;
+        this.lives = 3;
+    }
+    
+    public String getName() { return name; }
+    public int getScore() { return score; }
+    public int getLives() { return lives; }
+    
+    public void addScore(int points) { 
+        this.score += points; 
+    }
+    
+    public void loseLife() { 
+        this.lives--; 
+    }
+    
+    public boolean isAlive() { 
+        return lives > 0; 
+    }
+    
+    public void reset() {
+        this.score = 0;
+        this.lives = 3;
+    }
 }
