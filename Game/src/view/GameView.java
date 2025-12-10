6e21e544c7b1865ab6a9bb12b464c61cf4d34d6b
EldiRangel <eldi456.rangel@gmail.com>
2025-12-10 18:30:27 -0400

    Agregando Game View class
    Implemented GameView class to manage game UI components.

--- Game/src/view/GameView.java
+++ Game/src/view/GameView.java
@@ -1,3 +1,39 @@
+package view;
+
+import view.components.GamePanel;
+import view.components.ScorePanel;
+import javax.swing.*;
+import java.awt.*;
+
 public class GameView {
+    private JFrame frame;
+    private GamePanel gamePanel;
+    private ScorePanel scorePanel;
     
-}
+    public GameView() {
+        initializeFrame();
+    }
+    
+    private void initializeFrame() {
+        frame = new JFrame("Cazador de Fantasmas");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        
+        scorePanel = new ScorePanel();
+        gamePanel = new GamePanel();
+        
+        frame.add(scorePanel, BorderLayout.NORTH);
+        frame.add(gamePanel, BorderLayout.CENTER);
+        
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+    }
+    
+    public void show() {
+        frame.setVisible(true);
+    }
+    
+    public GamePanel getGamePanel() { return gamePanel; }
+    public ScorePanel getScorePanel() { return scorePanel; }
+    public JFrame getFrame() { return frame; }
+    }
